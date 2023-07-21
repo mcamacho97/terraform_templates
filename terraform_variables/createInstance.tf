b3fbb2ce6b09d9fcf60231d50f4fe563bc18ad20
mcamacho97 <mcamacho9729@gmail.com>
2023-07-20 19:41:17 -0600

    Lab: Variables with config files

--- terraform_variables/createInstance.tf
+++ terraform_variables/createInstance.tf
@@ -1,9 +1,8 @@
 resource "aws_instance" "MyFirstInstance" {
-  count         = 3
   ami           = "ami-0af9d24bd5539d7af"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "demoinstance-${count.index}"
+    Name = "demoinstance"
   }
 }
