2d8c4f01e5607e9fef75655003fd8f56ca6031b0
mcamacho97 <mcamacho9729@gmail.com>
2023-07-20 19:20:25 -0600

    feat: :sparkles: createInstance working

---
+++ create_EC2_instances/createInstance.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "MyFirstInstance" {
+  count         = 3
+  ami           = "ami-0af9d24bd5539d7af"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "demoinstance-${count.index}"
+  }
+}
